2e1908bfd20e5f29287a86e3a428ab24a03cbfbb
John Trowbridge <john@onna.com>
2020-12-01 16:45:48 +0000

    improved day 1 algorithms

--- src/day01.rs
+++ src/day01.rs
@@ -1,25 +1,30 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn part1(inp: String) {
   let nums = inp.lines().map(|c| c.parse().unwrap()).collect::<Vec<usize>>();
+  let mut seen = HashSet::new();
   for x in nums.iter() {
-    for y in nums.iter() {
-      if x + y == 2020 {
-        println!("{}", x*y);
-        return
-      }
+    let key = 2020-x;
+    if seen.contains(&key) {
+      println!("{}", key*x);
+      return
+    } else {
+      seen.insert(x);
     }
   }
 }
 
 pub fn part2(inp: String) {
   let nums = inp.lines().map(|c| c.parse().unwrap()).collect::<Vec<usize>>();
+  let mut seen = HashSet::new();
+  let mut seen2: HashMap<usize, usize> = HashMap::new();
   for x in nums.iter() {
-    for y in nums.iter() {
-      for z in nums.iter() {
-        if x + y + z == 2020 {
-          println!("{}", x*y*z);
-          return
-        }
-      }
+    let key = 2020-x;
+		seen.iter().map(|s| seen2.insert(x+s, x*s)).count();
+    if seen2.contains_key(&key) {
+      println!("{}", seen2.get(&key).unwrap() * x);
+      return
     }
+		seen.insert(*x);
   }
 }
