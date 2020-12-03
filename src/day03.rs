0b5924aec9bda00893c5a1556efd60b0bc878dbe
John Trowbridge <john@onna.com>
2020-12-03 15:49:03 +0000

    [Day 03] fewer allocations

--- src/day03.rs
+++ src/day03.rs
@@ -1,6 +1,6 @@
-pub fn get_trees(tree_map: &Vec<&str>, rise: usize, run: usize) -> usize {
+pub fn get_trees(tree_map: &str, rise: usize, run: usize) -> usize {
     tree_map
-        .iter()
+        .lines()
         .enumerate()
         .filter(|(i, _)| i % rise == 0)
         .filter(|(i, line)| line.chars().nth((run * i / rise) % line.len()) == Some('#'))
@@ -8,16 +8,16 @@ pub fn get_trees(tree_map: &Vec<&str>, rise: usize, run: usize) -> usize {
 }
 
 pub fn part1(inp: String) {
-    let tree_map = inp.lines().collect::<Vec<&str>>();
-    println!("{}", get_trees(&tree_map, 1, 3));
+    println!("{}", get_trees(&inp, 1, 3));
 }
 
 pub fn part2(inp: String) {
-    let tree_map = inp.lines().collect::<Vec<&str>>();
-    let slope1 = get_trees(&tree_map, 1, 1);
-    let slope2 = get_trees(&tree_map, 1, 3);
-    let slope3 = get_trees(&tree_map, 1, 5);
-    let slope4 = get_trees(&tree_map, 1, 7);
-    let slope5 = get_trees(&tree_map, 2, 1);
-    println!("{}", slope1 * slope2 * slope3 * slope4 * slope5);
+    println!(
+        "{}",
+        get_trees(&inp, 1, 1)
+            * get_trees(&inp, 1, 3)
+            * get_trees(&inp, 1, 5)
+            * get_trees(&inp, 1, 7)
+            * get_trees(&inp, 2, 1)
+    );
 }
