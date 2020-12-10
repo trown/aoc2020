e67d8092f1515fe4bf4235270e45d29566b9361c
John Trowbridge <alwaystherookie@gmail.com>
2020-12-10 00:30:32 -0500

    [Day 09] functionalize part 1 and remove panic from part 2

--- src/day09.rs
+++ src/day09.rs
@@ -1,50 +1,47 @@
-pub fn find_first_invalid(encrypted: Vec<u64>) -> u64 {
+use std::iter::repeat;
+
+pub fn find_first_invalid(preamble: usize, encrypted: Vec<u64>) -> u64 {
+    (preamble..encrypted.len())
+        .filter(|i| {
+            !repeat(i)
+                .zip(encrypted[i - preamble..*i].iter())
+                .any(|(i, j)| {
+                    j < &encrypted[*i]
+                        && encrypted[i - preamble..*i].contains(&(&encrypted[*i] - j))
+                })
+        })
+        .map(|i| encrypted[i])
+        .next()
+        .unwrap()
+}
+
+pub fn find_weakness(preamble: usize, encrypted: Vec<u64>) -> u64 {
+    let mut rev = encrypted.clone();
+    rev.reverse();
+    let first_invalid = find_first_invalid(preamble, encrypted);
     let mut found = 0;
-    for i in 25..1000 {
-        if found == 0 {
-            for j in encrypted[i - 25..i].iter() {
-                if j < &encrypted[i] && encrypted[i - 25..i].contains(&(&encrypted[i] - j)) {
-                    found = *j;
+
+    for (i, x) in rev.iter().enumerate() {
+        if found == 0 && x < &first_invalid {
+            for j in i + 1..rev.len() {
+                let temp_sum = (rev[i..j + 1]).iter().sum::<u64>();
+                if found == 0 && temp_sum == first_invalid {
+                    let max = &rev[i..j + 1].iter().max().unwrap();
+                    let min = &rev[i..j + 1].iter().min().unwrap();
+                    found = *max + *min;
                 }
-            }
-            if found == 0 {
-                found = encrypted[i];
-            } else {
-                found = 0;
             }
         }
     }
     found
 }
 
-pub fn find_weakness(encrypted: Vec<u64>) -> u64 {
-    let mut rev = encrypted.clone();
-    rev.reverse();
-    let first_invalid = find_first_invalid(encrypted);
-
-    for (i, x) in rev.iter().enumerate() {
-        if x < &first_invalid {
-            for j in i + 1..1000 {
-                let temp_sum = (rev[i..j + 1]).iter().sum::<u64>();
-                if temp_sum == first_invalid {
-                    let max = &rev[i..j + 1].iter().max().unwrap();
-                    let min = &rev[i..j + 1].iter().min().unwrap();
-
-                    println!("{}, {}, {}", max, min, *max + *min);
-                    panic!();
-                }
-            }
-        }
-    }
-    0
-}
-
 pub fn part1(inp: String) {
     let encrypted: Vec<u64> = inp.split("\n").map(|i| i.parse().unwrap()).collect();
-    println!("{}", find_first_invalid(encrypted));
+    println!("{}", find_first_invalid(25, encrypted));
 }
 
 pub fn part2(inp: String) {
     let encrypted: Vec<u64> = inp.split("\n").map(|i| i.parse().unwrap()).collect();
-    println!("{}", find_weakness(encrypted));
+    println!("{}", find_weakness(25, encrypted));
 }
