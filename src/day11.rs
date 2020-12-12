70c1f4641a92d310bcae7a60842cabc9979a2e1c
John Trowbridge <alwaystherookie@gmail.com>
2020-12-11 23:14:18 -0500

    Day 11

---
+++ src/day11.rs
@@ -0,0 +1,223 @@
+use std::cmp::min;
+use std::collections::HashSet;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum FloorPos {
+    Floor,
+    Empty,
+    Occupied,
+}
+
+impl From<char> for FloorPos {
+    fn from(c: char) -> Self {
+        match c {
+            '.' => FloorPos::Floor,
+            'L' => FloorPos::Empty,
+            '#' => FloorPos::Occupied,
+            _ => panic!(),
+        }
+    }
+}
+
+impl FloorPos {
+    pub fn is_occupied(&self) -> usize {
+        match self {
+            FloorPos::Occupied => 1,
+            _ => 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Layout {
+    state: Vec<Vec<FloorPos>>,
+    seen: HashSet<Vec<Vec<FloorPos>>>,
+}
+
+impl Layout {
+    fn new(state: Vec<Vec<FloorPos>>) -> Self {
+        let mut seen = HashSet::new();
+        seen.insert(state.clone());
+        Layout { state, seen }
+    }
+    fn tick(&mut self, f: bool) -> Option<Vec<Vec<FloorPos>>> {
+        let mut swap = vec![vec![FloorPos::Floor; self.state[0].len()]; self.state.len()];
+        for i in 0..self.state.len() {
+            for j in 0..self.state[0].len() {
+                let fp = self.state[i][j];
+                let occupied_neighbors = match f {
+                    true => self.occupied_neighbor_count(i, j),
+                    false => self.occupied_neighbor_count_part2(i, j),
+                };
+
+                let next_pos = match (fp, occupied_neighbors, f) {
+                    (FloorPos::Empty, 0, _) => FloorPos::Occupied,
+                    (FloorPos::Occupied, x, true) if x >= 4 => FloorPos::Empty,
+                    (FloorPos::Occupied, x, false) if x >= 5 => FloorPos::Empty,
+                    (otherwise, _, _) => otherwise,
+                };
+
+                swap[i][j] = next_pos;
+            }
+        }
+        self.state = swap;
+        if self.seen.insert(self.state.clone()) {
+            None
+        } else {
+            Some(self.state.clone())
+        }
+    }
+
+    fn occupied_neighbor_count(&self, i: usize, j: usize) -> usize {
+        let (mut n, mut s, mut e, mut w, mut ne, mut nw, mut se, mut sw) = (0, 0, 0, 0, 0, 0, 0, 0);
+
+        if i > 0 {
+            n = self.state[i - 1][j].is_occupied();
+        }
+        if i < self.state.len() - 1 {
+            s = self.state[i + 1][j].is_occupied();
+        }
+        if j < self.state[0].len() - 1 {
+            e = self.state[i][j + 1].is_occupied();
+        }
+        if j > 0 {
+            w = self.state[i][j - 1].is_occupied();
+        }
+        if i > 0 && j < self.state[0].len() - 1 {
+            ne = self.state[i - 1][j + 1].is_occupied();
+        }
+        if i > 0 && j > 0 {
+            nw = self.state[i - 1][j - 1].is_occupied();
+        }
+        if i < self.state.len() - 1 && j < self.state[0].len() - 1 {
+            se = self.state[i + 1][j + 1].is_occupied();
+        }
+        if i < self.state.len() - 1 && j > 0 {
+            sw = self.state[i + 1][j - 1].is_occupied();
+        }
+
+        n + s + e + w + ne + nw + se + sw
+    }
+
+    fn occupied_neighbor_count_part2(&self, i: usize, j: usize) -> usize {
+        let (mut n, mut s, mut e, mut w, mut ne, mut nw, mut se, mut sw) = (0, 0, 0, 0, 0, 0, 0, 0);
+
+        if i > 0 {
+            let o = (1..i + 1)
+                .map(|x| self.state[i - x][j])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                n = o.unwrap().is_occupied();
+            }
+        }
+        if i < self.state.len() - 1 {
+            let o = (1..self.state.len() - i)
+                .map(|x| self.state[i + x][j])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                w = o.unwrap().is_occupied();
+            }
+        }
+        if j < self.state[0].len() - 1 {
+            let o = (1..self.state[0].len() - j)
+                .map(|x| self.state[i][j + x])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                e = o.unwrap().is_occupied();
+            }
+        }
+        if j > 0 {
+            let o = (1..j + 1)
+                .map(|x| self.state[i][j - x])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                s = o.unwrap().is_occupied();
+            }
+        }
+        if i > 0 && j < self.state[0].len() - 1 {
+            let o = (1..min(i + 1, self.state[0].len() - j))
+                .map(|x| self.state[i - x][j + x])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                ne = o.unwrap().is_occupied();
+            }
+        }
+        if i > 0 && j > 0 {
+            let o = (1..min(i + 1, j + 1))
+                .map(|x| self.state[i - x][j - x])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                nw = o.unwrap().is_occupied();
+            }
+        }
+        if i < self.state.len() - 1 && j < self.state[0].len() - 1 {
+            let o = (1..min(self.state.len() - i, self.state[0].len() - j))
+                .map(|x| self.state[i + x][j + x])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                se = o.unwrap().is_occupied();
+            }
+        }
+        if i < self.state.len() - 1 && j > 0 {
+            let o = (1..min(self.state.len() - i, j + 1))
+                .map(|x| self.state[i + x][j - x])
+                .filter(|state| *state != FloorPos::Floor)
+                .next();
+            if o.is_some() {
+                sw = o.unwrap().is_occupied();
+            }
+        }
+
+        n + s + e + w + ne + nw + se + sw
+    }
+}
+
+pub fn part1(inp: String) {
+    let mut layout = Layout::new(
+        inp.split("\n")
+            .map(|row| row.chars().map(|c| FloorPos::from(c)).collect::<Vec<_>>())
+            .collect::<Vec<_>>(),
+    );
+    loop {
+        if let Some(state) = layout.tick(true) {
+            println!(
+                "{}",
+                state
+                    .iter()
+                    .flatten()
+                    .filter(|pos| **pos == FloorPos::Occupied)
+                    .count()
+            );
+            return ();
+        }
+    }
+}
+
+pub fn part2(inp: String) {
+    let mut layout = Layout::new(
+        inp.split("\n")
+            .map(|row| row.chars().map(|c| FloorPos::from(c)).collect::<Vec<_>>())
+            .collect::<Vec<_>>(),
+    );
+
+    loop {
+        if let Some(state) = layout.tick(false) {
+            println!(
+                "{}",
+                state
+                    .iter()
+                    .flatten()
+                    .filter(|pos| **pos == FloorPos::Occupied)
+                    .count()
+            );
+            return ();
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub mod day07;
 pub mod day08;
 pub mod day09;
 pub mod day10;
+pub mod day11;
 
 pub fn noop(_inp: String) {}
 
@@ -29,6 +30,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
         8 => (day08::part1, day08::part2),
         9 => (day09::part1, day09::part2),
         10 => (day10::part1, day10::part2),
+        11 => (day11::part1, day11::part2),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
